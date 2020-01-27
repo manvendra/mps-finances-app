e9c4b643fb809b8015c6782fe58ca249ae3a943e
manvendrapsingh <manvendra.singh@gmail.com>
2020-01-27 03:56:45 -0500

    updated code Check-in

--- finance-data-service/src/main/java/com/mps/finances/data/repository/jpa/FinanceDataJpaRepository.java
+++ finance-data-service/src/main/java/com/mps/finances/data/repository/jpa/FinanceDataJpaRepository.java
@@ -4,11 +4,9 @@ import com.mps.finances.data.repository.FinanceDataRepository;
 import com.mps.finances.data.repository.jpa.entities.account.FinancialAccount;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 @ConditionalOnProperty(value = "com.mps.finance.jpa.repo.enabled", havingValue = "true")
 public interface FinanceDataJpaRepository extends JpaRepository<FinancialAccount, Long>, FinanceDataRepository {
 
