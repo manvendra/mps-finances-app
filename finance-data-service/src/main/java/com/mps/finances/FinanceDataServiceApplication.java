40de5a927729dcd95a2490aabb8fb74797c5620a
manvendrapsingh <manvendra.singh@gmail.com>
2020-01-27 05:22:02 -0500

    updated code Check-in

--- finance-data-service/src/main/java/com/mps/finances/FinanceDataServiceApplication.java
+++ finance-data-service/src/main/java/com/mps/finances/FinanceDataServiceApplication.java
@@ -3,9 +3,11 @@ package com.mps.finances;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.mps.finances.data.repository.jpa")
 @SpringBootApplication
 public class FinanceDataServiceApplication {
