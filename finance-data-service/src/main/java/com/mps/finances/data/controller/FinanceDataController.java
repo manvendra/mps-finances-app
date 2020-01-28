e133c80af6d983feeb06ffc21fe9b4647b3bfbc2
manvendrapsingh <manvendra.singh@gmail.com>
2020-01-28 01:34:49 -0500

    updated code Check-in

--- finance-data-service/src/main/java/com/mps/finances/data/controller/FinanceDataController.java
+++ finance-data-service/src/main/java/com/mps/finances/data/controller/FinanceDataController.java
@@ -3,15 +3,13 @@ package com.mps.finances.data.controller;
 import com.mps.finances.account.FinancialAccount;
 import com.mps.finances.data.service.FinanceDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@RestController("data/api")
+@RestController
+@RequestMapping("data/FinancialAccount/")
 public class FinanceDataController {
 
     @Autowired
