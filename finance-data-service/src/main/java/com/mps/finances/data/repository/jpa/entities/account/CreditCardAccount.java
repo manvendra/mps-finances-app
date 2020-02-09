8b84b255f0aff9bffee8235486b048172f5928c2
manvendrapsingh <manvendra.singh@gmail.com>
2020-02-08 23:56:58 -0500

    updated code Check-in

--- finance-data-service/src/main/java/com/mps/finances/data/repository/jpa/entities/account/CreditCardAccount.java
+++ finance-data-service/src/main/java/com/mps/finances/data/repository/jpa/entities/account/CreditCardAccount.java
@@ -10,7 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
